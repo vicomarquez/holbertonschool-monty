0aa1dc31cef3686fe6a9d71e831919f96027a1d1
vicomarquez <vicomarquezmuniz@gmail.com>
2022-08-18 07:16:22 -0700

    push everything

--- opcodes.c
+++ opcodes.c
@@ -40,7 +40,7 @@ void _pall(stack_t **stack, unsigned int line_number)
 }
 
 /**
- * _pint -  prints the value at the top of the stack, followed by a new line
+ * _pint - prints the value at the top of the stack, followed by a new line
  * @stack: double pointer
  * @line_number: unsigned int
  */
