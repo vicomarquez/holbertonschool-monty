0ca569088bfa7e69274322a692983704ed5bf2be
vicomarquez <vicomarquezmuniz@gmail.com>
2022-08-21 18:59:55 -0700

    save changes

--- run_monty.c
+++ run_monty.c
@@ -7,11 +7,13 @@
 int main(int argc, char **argv)
 {
 	FILE *txt;
-	char *line = NULL, *head = NULL;
+	char *line = NULL;
+	char *head = NULL;
 	char buffer[1024];
 	stack_t *stack = NULL;
 	unsigned int numb_line = 0;
-	void (*f)(stack_t **, unsigned int);
+	char *a = 0;
+	void (*f)(stack_t **, unsigned int, char *);
 
 	if (argc != 2)
 	{
@@ -25,26 +27,21 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Error: Can't open file txt\n");
 		exit(EXIT_FAILURE);
 	}
-	while (fgets(buffer, 1024, txt) == -1)
+	while (fgets(buffer, 1024, txt) == NULL)
 	{
 		numb_line++;
 		line = strtok(buffer, "\n");
-		if (!Line)
+		if (!line)
 			continue;
-		head = strtok(line, "\t\r");
+		head = strtok(line, " \t\r");
 		if (head)
 		{
-			f = get_op(head, numb_line);
+			f = get_op(head, numb_line, a);
 			if (f)
-				f(&stack, numb_line);
+				f(&stack, numb_line, a);
 		}
 	}
-	free_dlistint(stack);
+	free_list(stack);
 	fclose(txt);
-<<<<<<< HEAD
 	return (0);
-	}
-=======
-return (0);
->>>>>>> 5f5aa07669f2c08765a20582bd46531ff849c64b
 }
